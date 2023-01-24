7f39aa973de1895815030271dd7dee7bf1f532dc
Salman_JS <salman4javascript@gmail.com>
2023-01-24 23:55:06 +0530

    New changes

--- controllers/qrcode_generator.js
+++ controllers/qrcode_generator.js
@@ -1,8 +1,4 @@
 const qrcode = require('qrcode');
-const { createCanvas, loadImage } = require("canvas");
-const fs = require('fs');
-const path = require("path");
-
 
 const options = {
   errorCorrectionLevel : 'H',
@@ -15,15 +11,8 @@ const options = {
   },
 }
 
-// Image convertor.
-function base64_encode() {
-  const file = path.join(__dirname, './Brew_LodZee.jpeg')
-  return "data:image/gif;base64,"+fs.readFileSync(file, 'base64')
-}
-
 const generator = async (req,res,next) => {
   // With image qr code
-  var image_qr = await canvas_generator();
   const option = JSON.stringify(`https://brew-classic.vercel.app/${req.body.room_spec_id}/static`);
   const optionWithoutQuotes = option.replace(/['"]/g, '');
   qrcode.toDataURL(optionWithoutQuotes, {type : 'terminal'},
@@ -44,40 +33,9 @@ const generator = async (req,res,next) => {
   )
 }
 
-async function create(dataForQRcode, center_image, width, cwidth) {
-  const canvas = createCanvas(width, width);
-  qrcode.toCanvas(
-    canvas,
-    dataForQRcode,
-    {
-      errorCorrectionLevel: "H",
-      margin: 1,
-      color: {
-        dark: "#000000",
-        light: "#ffffff",
-      },
-    }
-  );
 
-  const ctx = canvas.getContext("2d");
-  const img = await loadImage(center_image);
-  const center = (width - cwidth) / 2;
-  ctx.drawImage(img, center, center, cwidth, cwidth);
-  return canvas.toDataURL("image/png");
-}
-
-const canvas_generator = () => {
-  const convertedImage = base64_encode();
-  const image_qr = create(
-    "https://google.com",
-    convertedImage,
-    125,
-    50
-  );
-  return image_qr;
-}
 
 module.exports = {
-  generator, canvas_generator
+  generator
 }
 
