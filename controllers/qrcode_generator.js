7f887a505e3b3c89406d87754c9dac9eba9de021
Salman_JS <salman4javascript@gmail.com>
2023-01-24 22:54:38 +0530

    QR changes implemented

--- controllers/qrcode_generator.js
+++ controllers/qrcode_generator.js
@@ -1,4 +1,7 @@
 const qrcode = require('qrcode');
+const { createCanvas, loadImage } = require("canvas");
+const fs = require('fs');
+const path = require("path");
 
 
 const options = {
@@ -12,9 +15,19 @@ const options = {
   },
 }
 
-const generator = (req,res,next) => {
+// Image convertor.
+function base64_encode() {
+  const file = path.join(__dirname, './Brew_LodZee.jpeg')
+  return "data:image/gif;base64,"+fs.readFileSync(file, 'base64')
+}
+
+const generator = async (req,res,next) => {
+  // With image qr code
+  var image_qr = await canvas_generator();
   const option = JSON.stringify(`https://brew-classic.vercel.app/${req.body.room_spec_id}/static`);
-  qrcode.toDataURL(option, {type : 'terminal'},
+  const withoutQuotes = option.replace(/['"]/g, '');
+  // Non-Image QR code
+  qrcode.toDataURL(withoutQuotes, {type : 'terminal'},
     function(err, qrcode){
       if(err){
         console.log(err);
@@ -23,6 +36,7 @@ const generator = (req,res,next) => {
           message : "Some internal error occured"
         })
       }
+      // Change the message params based on the qr code needed!
       res.status(200).json({
         success : true,
         message : qrcode
@@ -31,7 +45,40 @@ const generator = (req,res,next) => {
   )
 }
 
+async function create(dataForQRcode, center_image, width, cwidth) {
+  const canvas = createCanvas(width, width);
+  qrcode.toCanvas(
+    canvas,
+    dataForQRcode,
+    {
+      errorCorrectionLevel: "H",
+      margin: 1,
+      color: {
+        dark: "#000000",
+        light: "#ffffff",
+      },
+    }
+  );
+
+  const ctx = canvas.getContext("2d");
+  const img = await loadImage(center_image);
+  const center = (width - cwidth) / 2;
+  ctx.drawImage(img, center, center, cwidth, cwidth);
+  return canvas.toDataURL("image/png");
+}
+
+const canvas_generator = () => {
+  const convertedImage = base64_encode();
+  const image_qr = create(
+    "https://google.com",
+    convertedImage,
+    125,
+    50
+  );
+  return image_qr;
+}
+
 module.exports = {
-  generator
+  generator, canvas_generator
 }
 
