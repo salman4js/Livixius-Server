5d3601a9af251397df4a1454bea07f5675b223f9
Salman_JS <salman4javascript@gmail.com>
2023-03-10 12:37:14 +0530

    Removed duplicates from the fav customers

--- controllers/userController.js
+++ controllers/userController.js
@@ -264,13 +264,24 @@ async function favCustomer(req, res, next){
 }
 
 async function checkFrequent(users){
+  
   const phoneNumbers = users.map(user => user.phonenumber);
   const count = phoneNumbers.reduce((acc, phoneNumber) => {
     acc[phoneNumber] = (acc[phoneNumber] || 0) + 1;
     return acc;
   }, {});
-  const frequentUsers = users.filter(user => user.dateofcheckout !== '' && count[user.phonenumber] >= 5);
-  return frequentUsers;
+
+  const frequentUsers = users.filter(user => count[user.phonenumber] >= 2 && user.dateofcheckout !== '');
+  
+  // Filter the user to remove duplicates as we take this data from the userdb which tends to have duplicates!
+  const filteredUsers = frequentUsers.reduce((acc, user) => {
+    const key = user.username + user.phonenumber;
+    if (!acc.has(key)) {
+      acc.set(key, user);
+    }
+    return acc;
+  }, new Map());
+  return [...filteredUsers.values()];
 }
 
 
