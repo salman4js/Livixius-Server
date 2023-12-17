e8b583acf8cabf386350c74ab3629674b76855c9
salman4js <salman4javascript@gmail.com>
2023-12-17 14:52:43 +0530

    Update path __dirname into process variables

--- content.methods/export.to.csv/export.to.csv.js
+++ content.methods/export.to.csv/export.to.csv.js
@@ -14,13 +14,14 @@ class ExportToCsv {
               status: false, creation: false, message: 'Some internal error occurred'
           }
       };
+        this.basePath = 'content.methods/export.to.csv/downloads';
     };
 
     // Get the path and header value ready for the CSV writer.
     _prepareCsvWriter(){
-        this.downloadPath = this.options.path + '/' + this.options.fileName;
+        this.downloadPath = this.basePath + '/' + this.options.fileName;
         this.writer = this.csvWriter.createObjectCsvWriter({
-            path: path.resolve(__dirname, this.downloadPath),
+            path: path.resolve(process.cwd(), this.downloadPath),
             header: this.options.headerValue
         });
     };
@@ -28,7 +29,7 @@ class ExportToCsv {
     // Check and clear the files in the provided filepath with the same filename to avoid unexpected behavior.
     _checkAndClearDownloadPath(){
         return new Promise((resolve) => {
-            fs.unlink(path.resolve(__dirname, this.downloadPath), (err) => {
+            fs.unlink(path.resolve(process.cwd(), this.downloadPath), (err) => {
                 resolve(); // Resolving even though we get an error while deleting the file in the provided path.
             });
         });
@@ -47,6 +48,7 @@ class ExportToCsv {
                reject(this.infoMessage.failure);
            });
            this.writer.writeRecords(this.options.cellValues).then(() => {
+               this.infoMessage.success['successWriterInstance'] = this.writer;
                resolve(this.infoMessage.success);
            }).catch((error) => {
                this.infoMessage.failure['errorInCsv'] = error;
